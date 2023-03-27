64d8e5b243d312d6ea2538e4f5b60683204be151
Zilba26 <basile.lebaudy@gmail.com>
2023-03-27 15:26:52 +0200

    Serveur api project finish

--- src/main/java/fr/zilba/restfulapi/service/VilleService.java
+++ src/main/java/fr/zilba/restfulapi/service/VilleService.java
@@ -10,10 +10,6 @@ public interface VilleService {
 
     List<City> getInfoCities(Map<String, String> params, String order);
 
-    default List<City> getInfoCities(String order) {
-        return getInfoCities(null, order);
-    }
-
     City createCity(City ville);
 
     City deleteCity(String codeCommune);
